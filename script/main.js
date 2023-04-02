a0248c3f222bb32f2bee846f841e0d6f46198713
nicolas <garcianico1998@gmail.com>
2023-04-02 18:47:26 -0300

    javascript

---
+++ script/main.js
@@ -0,0 +1,32 @@
+let hincha = prompt("De que equipo sos hincha?");
+const VACIO = "";
+const EQUIPO = "ESTUDIANTES";
+
+if (hincha.toUpperCase() == EQUIPO){
+    console.log("Podés ingresar al sitio libremente");
+}
+else if (hincha == VACIO){
+    console.log("Debes ingresar una respuesta");
+}
+else{
+    console.log("Debes respetar nuestras normas de privacidad y respeto para ingresar");
+}
+
+let pregunta = prompt("Veamos si conoces 7 banderas de Estudiantes. Queres jugar?");
+const RESPUESTA = "SI";
+const RESPNEG = "NO SE";
+if (pregunta.toUpperCase() == RESPUESTA){
+    let bandera = prompt("Ingresa el nombre de una bandera. En caso de no saber, ingresar NO SE");
+    if (bandera != VACIO && bandera.toUpperCase() != RESPNEG){
+    for (let i = 0; i <= 7; i++) {
+        bandera = prompt("Ingresa el nombre de una bandera. En caso de no saber, ingresar NO SE");
+        console.log("Muy bien! Conoces " + i + " banderas");
+        if (bandera.toUpperCase() == RESPNEG){
+            break
+        }
+        else if (bandera.toUpperCase() == VACIO){
+            break
+        }
+    }
+}
+}
